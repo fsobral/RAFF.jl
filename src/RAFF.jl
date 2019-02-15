ed34fd01b8b0870f55e89e7bccac8e0f887eff3c
Francisco <chicosobral@gmail.com>
2019-02-15 10:10:07 -0200

    Improve documentation of praff.

--- src/RAFF.jl
+++ src/RAFF.jl
@@ -525,37 +525,37 @@ function raff(model::Function, data::Array{Float64, 2}, n::Int; kwargs...)
 end
 
 """
+    praff(model::Function, data::Array{Float64, 2}, n::Int; MAXMS::Int=1,
+          SEEDMS::Int=123456789, batches::Int=1, initguess=zeros(Float64, n),
+          ε=1.0e-4)
 
-    praff(model::Function, data::Array{Float64, 2}, n::Int;
-          MAXMS::Int=1, SEEDMS::Int=123456789, batches::Int=1 )
+    praff(model::Function, gmodel!::Function, data::Array{Float64, 2}, n::Int;
+          MAXMS::Int=1, SEEDMS::Int=123456789, batches::Int=1,
+          initguess=zeros(Float64, n), ε::Float64=1.0e-4)
 
-    praff(model::Function, gmodel!::Function,
-          data::Array{Float64, 2}, n::Int; MAXMS::Int=1,
-          SEEDMS::Int=123456789, batches::Int=1 )
-
-Multicore shared memory version of RAFF. See the description of the
-[raff](@ref) function for the main (non-optional) arguments.
+Multicore distributed version of RAFF. See the description of the
+[`raff`](@ref) function for the main (non-optional) arguments. All the
+communication is performed by channels.
 
 This function uses all available **local** workers to run RAFF
 algorithm. Note that this function does not use *Tasks*, so all the
-parallelism is based on the `Distributed` package.
+parallelism is based on the [Distributed](https://docs.julialang.org/en/latest/manual/parallel-computing/#Multi-Core-or-Distributed-Processing-1) package.
 
 The optional arguments are
 
   - `MAXMS`: number of multistart points to be used
   - `SEEDMS`: integer seed for random multistart points
   - `batches`: size of batches to be send to each worker
+  - `initguess`: starting point to be used in the multistart procedure
+  - `ε`: stopping tolerance
 
-Returns a tuple `x`, `f`, `p` where
-
-  - `x` is the solution
-  - `f` is the value of the error at the solution
-  - `p` is the number of trusted points found by the voting system.
+Returns a [RaffOutput](@ref) object containing the solution.
 
 """
 function praff(model::Function, gmodel!::Function,
                data::Array{Float64, 2}, n::Int; MAXMS::Int=1,
-               SEEDMS::Int=123456789, batches::Int=1)
+               SEEDMS::Int=123456789, batches::Int=1, initguess=zeros(Float64, n),
+               ε::Float64=1.0e-4)
 
     # Initialize random generator
     seedMS = MersenneTwister(SEEDMS)
