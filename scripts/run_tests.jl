402468cfb7f5ad861cbc65e59f7c1de0faa35e51
Francisco <chicosobral@gmail.com>
2019-03-13 17:23:11 -0300

    Read all test problems from directory

--- scripts/run_tests.jl
+++ scripts/run_tests.jl
@@ -1,47 +1,82 @@
+# This script runs all the tests and create a table for comparison.
+
 using RAFF
 using Printf
 using DelimitedFiles
 
 fpath = "../test/test-problems/"
-fname = "CI1"
-mtype = "circle"
 
-open(fpath * fname * ".txt") do fp
-        
-    global N = parse(Int, readline(fp))
-        
-    global data = readdlm(fp)
-        
+getType = begin
+
+    d = Dict(
+        "C" => "cubic",
+        "L" => "linear",
+        "LOG" => "logistic",
+        "CI" => "circle",
+        "E" => "expon"
+    )
+
+    (s) -> d[s]
+
 end
 
-n, model, = RAFF.model_list[mtype]
-
-# Retrieve the true outliers
-outliers = findall(data[:, end] .== 1.0)
-
-# Run RAFF
-rsol, t, = @timed raff(model, data[:, 1:end - 1], n)
-
-open("/tmp/table.csv", "a") do fp
-
-    @printf(fp, "%5s\t[", fname)
-
-    for i = 1:n - 1
-
-        @printf(fp, "%10.3e, ", rsol.solution[i])
-
-    end
-
-    @printf(fp, "%10.3e]\t", rsol.solution[n])
-
-    nexact = 0
-
-    for i in rsol.outliers
-
-        (i in outliers) && (nexact += 1)
-
-    end
+function run_tests(;kwargs...)
     
-    @printf(fp, "%3d\t%3d\t%10.4f\n", length(rsol.outliers), nexact, t)
+    for fname in readdir(fpath)
+
+        occursin("sol", fname) && continue
+
+        m = match(r"[^1-9]*", fname)
+
+        (m == nothing) && continue
+
+        mtype = getType(m.match)
+
+        open(fpath * fname) do fp
+
+            global N = parse(Int, readline(fp))
+
+            global data = readdlm(fp)
+
+        end
+
+        n, model, = RAFF.model_list[mtype]
+
+        np, = size(data)
+        
+        # Retrieve the true outliers
+        outliers = findall(data[:, end] .== 1.0)
+
+        # Run RAFF
+        rsol, t, = @timed raff(model, data[:, 1:end - 1], n; kwargs...)
+
+        open("/tmp/table.csv", "a") do fp
+
+            name = match(r"[^.]*", fname).match
+            
+            @printf(fp, "%5s\t%6d\t%3d\t[", name, np, length(outliers))
+
+            for i = 1:n - 1
+
+                @printf(fp, "%10.3e, ", rsol.solution[i])
+
+            end
+
+            @printf(fp, "%10.3e]\t", rsol.solution[n])
+
+            nexact = 0
+
+            for i in rsol.outliers
+
+                (i in outliers) && (nexact += 1)
+
+            end
+
+            @printf(fp, "%3d\t%3d\t%10.4f\t%d\n",
+                    length(rsol.outliers), nexact, t, rsol.status)
+
+        end
+
+    end
 
 end
