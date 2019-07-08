12ed522026433a06163ca85785d1aa35ca113e17
Francisco <chicosobral@gmail.com>
2019-07-08 18:44:21 -0300

    Improve example of circle detection.

--- test/scripts/draw_and_detect.jl
+++ test/scripts/draw_and_detect.jl
@@ -1,4 +1,6 @@
-# This example shows how to get mouse clicks in Julia
+# This example shows how to use RAFF.jl to detect circles drawn from
+# the user. This example was inspired in `GtkReactive.jl` drawing
+# example.
 
 # Packages needed
 #
@@ -10,14 +12,64 @@
 using Gtk, Gtk.ShortNames, GtkReactive, Graphics, Colors
 using RAFF
 
-win = Window("Drawing")
-c = canvas(UserUnit)       # create a canvas with user-specified coordinates
-push!(win, c)
+win = Window("Drawing") |> (bx = Box(:v))
+
+set_gtk_property!(bx, :spacing, 10)
+
+cb = GtkReactive.dropdown(["RAFF", "Least Squares"])
+push!(bx, cb)
+
+c = canvas(UserUnit, 200, 200)       # create a canvas with user-specified coordinates
+push!(bx, c)
 
 const moddraw = Signal([]) # the model points
 const newline = Signal([]) # the in-progress line (will be added to list above)
 
 const drawing = Signal(false)  # this will become true if we're actively dragging
+
+choice = map(x->(push!(moddraw, []);value(x)), cb)
+
+function run_raff()
+
+    n, model, = RAFF.model_list["circle"]
+
+    np, = size(value(newline))
+
+    data = Array{Float64, 2}(undef, np, 3)
+
+    l = value(newline)
+    
+    for i in 1:np
+
+        data[i,1] = l[i].x
+        data[i,2] = l[i].y
+        data[i,3] = 0.0
+
+    end
+
+    r = if value(choice) == "RAFF"
+        
+        raff(model, data, n, ftrusted=0.7, MAXMS=10)
+
+    else
+
+        raff(model, data, n, ftrusted=1.0, MAXMS=10)
+
+    end
+    println(r)
+    # Build points for drawing the circle
+    p = (α) -> [abs(r.solution[3]) * cos(α) + r.solution[1], abs(r.solution[3]) * sin(α) + r.solution[2]]
+    
+    push!(moddraw, [p(i) for i in LinRange(0.0, 2 * π, 100)])
+    
+end
+
+# If has changed the selection box, then run RAFF again.
+sigc = map(choice) do c
+
+    run_raff()
+    
+end
 
 sigstart = map(c.mouse.buttonpress) do btn
     # This is the beginning of the function body, operating on the argument `btn`
@@ -38,33 +90,7 @@ end
 sigend = map(c.mouse.buttonrelease) do btn
     if btn.button == 1
         push!(drawing, false)  # deactivate dragging
-        # append our new line to the overall list
-
-        n, model, = RAFF.model_list["circle"]
-
-        np, = size(value(newline))
-
-        data = Array{Float64, 2}(undef, np, 3)
-
-        l = value(newline)
-        
-        for i in 1:np
-
-            data[i,1] = l[i].x
-            data[i,2] = l[i].y
-            data[i,3] = 0.0
-
-        end
-        
-        r = raff(model, data, n, ftrusted=0.7, MAXMS=10)
-        println(r)
-        
-        # Build points for drawing the circle
-        p = (α) -> [abs(r.solution[3]) * cos(α) + r.solution[1], abs(r.solution[3]) * sin(α) + r.solution[2]]
-        push!(moddraw, [p(0)])
-        for α in LinRange(0.0, 2 * π, 100)
-            push!(moddraw, push!(value(moddraw), p(α)))
-        end
+        run_raff()
     end
 end
 
@@ -92,7 +118,6 @@ function drawcircle(ctx, l, color)
     stroke(ctx)
 end
     
-
 # Because `draw` isn't a one-line function, we again use do-block syntax:
 redraw = draw(c, moddraw, newline) do cnvs, circ, newl  # the function body takes 3 arguments
     fill!(cnvs, colorant"white")   # set the background to white
